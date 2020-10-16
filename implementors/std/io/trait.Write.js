9bc7db0f174d964623c8f1f53c7a8122cb68bcb1
jplatte <jplatte@users.noreply.github.com>
2020-10-16 18:45:37 +0000

    deploy: 2b33c97117355cdc599aa29fb1a64b8016ee3f68

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["env_logger"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"env_logger/fmt/struct.Formatter.html\" title=\"struct env_logger::fmt::Formatter\">Formatter</a>","synthetic":false,"types":["env_logger::fmt::Formatter"]}];
+implementors["env_logger"] = [{"text":"impl Write for Formatter","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
